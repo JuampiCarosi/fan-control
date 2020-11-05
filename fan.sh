d50efc32d69ff36ebe628bd74f932242087b24f0
juampapo546 <juampicarosi@gmail.com>
2020-11-05 12:09:50 -0300

    Bring 3 fan support and reorganize code
    I reorganized code for a clearer view and renamed all variables to their fan name to bring support in the future to other macs

--- fan.sh
+++ fan.sh
@@ -1,89 +1,145 @@
+#Define which fan is what with the labels
+fan1_label='/sys/devices/platform/applesmc.768/fan1_label'
+label1=$(cat $fan1_label | tr '[:upper:]' '[:lower:]') 
+
+fan2_label='/sys/devices/platform/applesmc.768/fan2_label'
+label2=$(cat $fan2_label | tr '[:upper:]' '[:lower:]') 
+
+fan3_label='/sys/devices/platform/applesmc.768/fan3_label'
+label3=$(cat $fan3_label | tr '[:upper:]' '[:lower:]') 
+
 #Getting values from applemc
-fan_hdd_control_file='/sys/devices/platform/applesmc.768/fan2_manual'
-hdd_manual=$(cat $fan_hdd_control_file)
 
-fan_cpu_control_file='/sys/devices/platform/applesmc.768/fan3_manual'
-cpu_manual=$(cat $fan_cpu_control_file)
+## FAN 1
 
-fan_hdd_max_file='/sys/devices/platform/applesmc.768/fan2_max'
-hdd_max=$(cat $fan_hdd_max_file)
+fan1_control_file='/sys/devices/platform/applesmc.768/fan1_manual'
+fan1_manual=$(cat $fan1_control_file)
 
-fan_hdd_min_file='/sys/devices/platform/applesmc.768/fan2_min'
-hdd_min=$(cat $fan_hdd_min_file)
+fan1_max_file='/sys/devices/platform/applesmc.768/fan1_max'
+fan1_max=$(cat $fan1_max_file)
 
-fan_cpu_max_file='/sys/devices/platform/applesmc.768/fan3_max'
-cpu_max=$(cat $fan_cpu_max_file)
+fan1_min_file='/sys/devices/platform/applesmc.768/fan1_min'
+fan1_min=$(cat $fan1_min_file)
 
-fan_cpu_min_file='/sys/devices/platform/applesmc.768/fan3_min'
-cpu_min=$(cat $fan_cpu_min_file)
+fan1_current_output_file='/sys/devices/platform/applesmc.768/fan1_output'
 
-fan_cpu_current_output_file='/sys/devices/platform/applesmc.768/fan3_output'
+##FAN 2
 
-fan_hdd_current_output_file='/sys/devices/platform/applesmc.768/fan2_output'
+fan2_control_file='/sys/devices/platform/applesmc.768/fan2_manual'
+fan2_manual=$(cat $fan2_control_file)
 
-# Putting fan 2 (hdd) and fan 3 (cpu) on manual mode or set all to auto again
-if [ $cpu_manual="0" ]; then
-    echo "1" > $fan_cpu_control_file
+fan2_max_file='/sys/devices/platform/applesmc.768/fan2_max'
+fan2_max=$(cat $fan2_max_file)
+
+fan2_min_file='/sys/devices/platform/applesmc.768/fan2_min'
+fan2_min=$(cat $fan2_min_file)
+
+fan2_current_output_file='/sys/devices/platform/applesmc.768/fan2_output'
+
+##FAN 3
+
+fan3_control_file='/sys/devices/platform/applesmc.768/fan3_manual'
+fan3_manual=$(cat $fan3_control_file)
+
+fan3_max_file='/sys/devices/platform/applesmc.768/fan3_max'
+fan3_max=$(cat $fan3_max_file)
+
+fan3_min_file='/sys/devices/platform/applesmc.768/fan3_min'
+fan3_min=$(cat $fan3_min_file)
+
+fan3_current_output_file='/sys/devices/platform/applesmc.768/fan3_output'
+
+
+# Putting fans on manual mode or set all to auto again
+if [ $fan1_manual = "0" ]; then
+    echo "1" > $fan1_control_file
 fi
 
-if [ $hdd_manual = "0" ]; then
-    echo "1" > $fan_hdd_control_file
+if [ $fan2_manual="0" ]; then
+    echo "1" > $fan2_control_file
 fi
+
+if [ $fan3_manual="0" ]; then
+    echo "1" > $fan3_control_file
+fi
+
 
 if [ $1 = auto ]; then
-    echo "0" > $fan_cpu_control_file
-    echo "0" > $fan_hdd_control_file
-fi
-
-#### START CPU CONTROL
-if [ $1 =  "cpu" ]; then
-    cpu_input=$2
-
-    # Calculating the net value that will be given to the cpu fan
-    cpu_100=$(($cpu_max-$cpu_min))
-
-    # Calculating percentage of cpu value
-    cpu_net=$(($cpu_input*$cpu_100/100))
-    cpu_final=$(($cpu_net+$cpu_min))
-
-    # Switch back cpu to auto mode
-    if [ $2 = "auto" ]; then
-      echo "0" > $fan_cpu_control_file
-      echo "cpu mode set to auto"
-      else
-
-      # Writing the final value to the cpu files
-      echo $cpu_final > $fan_cpu_current_output_file
-      echo "cpu fan set to"  $cpu_final "rpm"
-    fi
+    echo "0" > $fan1_control_file
+    echo "0" > $fan2_control_file
+    echo "0" > $fan3_control_file
 
 fi
 
-#### START HDD CONTROL
-if [ "$1" = "hdd" ]; then
-    hdd_input=$2
+#### START FAN 1 CONTROL
+if [ "$1" = "fan1" ]; then
+    fan1_input=$2
 
     # Calculating the net value that will be given to the fans
-    hdd_100=$(($hdd_max-$hdd_min))
+    fan1_100=$(($fan1_max-$fan1_min))
 
     # Calculating final percentage value
-    hdd_net=$(($hdd_input*$hdd_100/100))
-    hdd_final=$(($hdd_net+$hdd_min))
+    fan1_net=$(($fan1_input*$fan1_100/100))
+    fan1_final=$(($fan1_net+$fan1_min))
 
-    # Switch back hdd to auto mode
+    # Switch back fan1 to auto mode
     if [ $2 = "auto" ]; then
-      echo "0" > $fan_hdd_control_file
-      echo "hdd fan mode set to auto"
+      echo "0" > $fan1_control_file
+      echo "fan1 fan mode set to auto"
       else
 
       # Writing the final value to the applemc files
-      echo $hdd_final > $fan_hdd_current_output_file && echo "hdd fan set to" $hdd_final "rpm" || echo  "
+      echo $fan1_final > $fan1_current_output_file && echo "fan1 fan set to" $fan1_final "rpm" || echo  "
 Try running command as sudo"
 
     fi
 
 fi
 
-if [ $1 = "-h"  ]; then
-    echo "fan [ SELECT FAN TO CONTROL ( cpu, hdd or auto to set both fans to auto )] [SELECT THE PERCENTAGE YOU WANT THE FAN TO RUN ( value from 1 to 100, just the number | type auto to set automatic control)]"
+#### START FAN 2 CONTROL
+if [ $1 =  "fan2" ]; then
+    fan2_input=$2
+
+    # Calculating the net value that will be given to the fan2 fan
+    fan2_100=$(($fan2_max-$fan2_min))
+
+    # Calculating percentage of fan2 value
+    fan2_net=$(($fan2_input*$fan2_100/100))
+    fan2_final=$(($fan2_net+$fan2_min))
+
+    # Switch back fan2 to auto mode
+    if [ $2 = "auto" ]; then
+      echo "0" > $fan2_control_file
+      echo "fan2 mode set to auto"
+      else
+
+      # Writing the final value to the fan2 files
+      echo $fan2_final > $fan1_current_output_file && echo "fan2 fan set to" $fan2_final "rpm" || echo  "
+Try running command as sudo"
+    fi
+
+fi
+
+## START FAN 3 CONTROL
+if [ $1 =  "fan3" ]; then
+    fan3_input=$2
+
+    # Calculating the net value that will be given to the fan2 fan
+    fan3_100=$(($fan3_max-$fan3_min))
+
+    # Calculating percentage of fan2 value
+    fan3_net=$(($fan3_input*$fan3_100/100))
+    fan3_final=$(($fan3_net+$fan3_min))
+
+    # Switch back fan2 to auto mode
+    if [ $2 = "auto" ]; then
+      echo "0" > $fan2_control_file
+      echo "fan3 mode set to auto"
+      else
+
+      # Writing the final value to the fan3 files
+      echo $fan3_final > $fan3_current_output_file && echo "fan3 fan set to" $fan3_final "rpm" || echo  "
+Try running command as sudo"
+    fi
+
 fi
