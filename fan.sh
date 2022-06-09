d53070def77a8753da2933c85b22daef17d33ef6
Bruno Raoult <braoult@gmail.com>
2022-06-09 15:05:17 +0200

    make shellcheck happy, remove duplicate fan functions (untested!)

--- fan.sh
+++ fan.sh
@@ -2,220 +2,112 @@
 #
 # forked from https://github.com/juampapo546/fan-control
 
+#CMD="${0##*/}"
+sysdir="/sys/devices/platform/applesmc.768"
+
 #Define input variable for the functions
 input=$2
 
+declare -a fan_control_file fan_label label
+
 #Match labels with fan number and get control files
-fan1_control_file='/sys/devices/platform/applesmc.768/fan1_manual'
-fan1_label='/sys/devices/platform/applesmc.768/fan1_label'
-label1=$(cat $fan1_label | tr '[:upper:]' '[:lower:]')
+fan_control_file[1]="$sysdir/fan1_manual"
+fan_label[1]="$sysdir/fan1_label"
+label[1]=$(< ${fan_label[1]} )
+label[1]=${label[1],,}
 
-if [ $label1 != "exhaust" ] && [ $label1 != "master" ]; then
-	fan2_control_file='/sys/devices/platform/applesmc.768/fan2_manual'
-	fan2_label='/sys/devices/platform/applesmc.768/fan2_label'
-	label2=$(cat $fan2_label | tr '[:upper:]' '[:lower:]')
+if [[ "${label[1]}" != "exhaust" && "${label[1]}" != "master" ]]; then
+	fan_control_file[2]="$sysdir/fan2_manual"
+	fan_label[2]="$sysdir/fan2_label"
+    label[2]=$(< ${fan_label[1]} )
+    label[2]=${label[2],,}
 
-	fan3_control_file='/sys/devices/platform/applesmc.768/fan3_manual'
-	fan3_label='/sys/devices/platform/applesmc.768/fan3_label'
-	label3=$(cat $fan3_label | tr '[:upper:]' '[:lower:]')
+	fan_control_file[3]="$sysdir/fan3_manual"
+	fan_label[3]="$sysdir/fan3_label"
+    label[3]=$(< ${fan_label[1]} )
+    label[3]=${label[3],,}
 fi
 
-# Fan functions
-function_fan1 () {
-
+# fan() - set fan
+# argument is fan number (starting from 1)
+fan_function() {
+    local fan_manual fan_max_file fan_max fan_min_file fan_min
+    local fan_current_output_file
+    local -i fan_100 fan_net fan_final
+    fan="$1"
     # Getting fan files and data from applesmc.768
-    fan1_manual=$(cat $fan1_control_file)
+    fan_manual=$(cat ${fan_control_file[$fan]})
 
-    fan1_max_file='/sys/devices/platform/applesmc.768/fan1_max'
-    fan1_max=$(cat $fan1_max_file)
+    fan_max_file="$sysdir/fan${fan}_max"
+    fan_max=$(cat "$fan_max_file")
 
-    fan1_min_file='/sys/devices/platform/applesmc.768/fan1_min'
-    fan1_min=$(cat $fan1_min_file)
+    fan_min_file="$sysdir/fan${fan}_min"
+    fan_min=$(cat "$fan_min_file")
 
-    fan1_current_output_file='/sys/devices/platform/applesmc.768/fan1_output'
+    fan_current_output_file="$sysdir/fan${fan}_output"
 
     #Putting fan on manual mode
-    if [ $fan1_manual = "0" ]; then
-        echo "1" > $fan1_control_file
+    if [ "$fan_manual" = "0" ]; then
+        echo "1" > "${fan_control_file[$fan]}"
     fi
 
     # Calculating the net value that will be given to the fans
-    fan1_100=$(($fan1_max-$fan1_min))
+    fan_100=$((fan_max - fan_min))
     # Calculating final percentage value
-    fan1_net=$(($input*$fan1_100/100))
-    fan1_final=$(($fan1_net+$fan1_min))
+    fan_net=$((input * fan_100 / 100))
+    fan_final=$((fan_net + fan_min))
 
     # Switch back fan1 to auto mode
-    if [ $input = "auto" ]; then
-    echo "0" > $fan1_control_file
-    echo "fan mode set to auto"
+    if [ "$input" = "auto" ]; then
+        echo "0" > ${fan_control_file[$fan]}
+        printf "fan mode set to auto"
     else
 
-    # Writing the final value to the applemc files
-    echo $fan1_final > $fan1_current_output_file && echo "fan set to" $fan1_final "rpm" || echo  "
-        Try running command as sudo"
-
-    fi
-
-}
-
-function_fan2 () {
-
-    #Getting values from applemc.768
-    fan2_manual=$(cat $fan2_control_file)
-
-    fan2_max_file='/sys/devices/platform/applesmc.768/fan2_max'
-    fan2_max=$(cat $fan2_max_file)
-
-    fan2_min_file='/sys/devices/platform/applesmc.768/fan2_min'
-    fan2_min=$(cat $fan2_min_file)
-
-    fan2_current_output_file='/sys/devices/platform/applesmc.768/fan2_output'
-
-    # Putting fan on manual mode
-    if [ $fan2_manual = "0" ]; then
-        echo "1" > $fan2_control_file
-    fi
-
-    # Calculating the net value that will be given to the fan2 fan
-    fan2_100=$(($fan2_max-$fan2_min))
-
-    # Calculating percentage of fan2 value
-    fan2_net=$(($input*$fan2_100/100))
-    fan2_final=$(($fan2_net+$fan2_min))
-
-    # Switch back fan2 to auto mode
-    if [ $input = "auto" ]; then
-        echo "0" > $fan2_control_file
-        echo "fan mode set to auto"
-    else
-
-        # Writing the final value to the fan2 files
-        echo $fan2_final > $fan2_current_output_file && echo "fan set to" $fan2_final "rpm" || echo  "
-    Try running command as sudo"
+        # Writing the final value to the applemc files
+        if $fan_final > "$fan_current_output_file"; then
+            printf "fan set to %d rpm.\n" "$fan_final"
+        else
+            printf "Try running command as sudo\n"
+        fi
     fi
 }
-
-function_fan3 () {
-
-    #Getting values from applemc.768
-    fan3_manual=$(cat $fan3_control_file)
-
-    fan3_max_file='/sys/devices/platform/applesmc.768/fan3_max'
-    fan3_max=$(cat $fan3_max_file)
-
-    fan3_min_file='/sys/devices/platform/applesmc.768/fan3_min'
-    fan3_min=$(cat $fan3_min_file)
-
-    fan3_current_output_file='/sys/devices/platform/applesmc.768/fan3_output'
-
-     # Putting fan on manual mode
-    if [ $fan3_manual = "0" ]; then
-        echo "1" > $fan3_control_file
-    fi
-
-    # Calculating the net value that will be given to the fan2 fan
-    fan3_100=$(($fan3_max-$fan3_min))
-
-    # Calculating percentage of fan2 value
-    fan3_net=$(($input*$fan3_100/100))
-    fan3_final=$(($fan3_net+$fan3_min))
-
-    # Switch back fan3 to auto mode
-    if [ $input = "auto" ]; then
-    echo "0" > $fan3_control_file
-    echo "fan mode set to auto"
-    else
-
-    # Writing the final value to the fan3 files
-    echo $fan3_final > $fan3_current_output_file && echo "fan set to" $fan3_final "rpm" || echo  "
-    Try running command as sudo"
-    fi
-}
-
 
 # Commands
-case $1 in
-
+command="$1"
+case "$command" in
     "")
-        echo "Available fans:"
-        echo "  $label1"
-        if [ $label1 != "exhaust" ] && [ $label1 != "master" ]; then
-		echo "  $label2"
-		echo "  $label3"
-	fi
-    ;;
+        printf "Available fans:\n"
+        printf "  %s\n" "${label[1]}"
+        if [[ "${label[1]}" != "exhaust" && "${label[1]}" != "master" ]]; then
+            printf "  %s\n" "${label[2]}"
+            printf "  %s\n" "${label[3]}"
+	    fi
+        ;;
     ### AUTO CONTROL
     auto)
-        echo "0" > $fan1_control_file
-        if [ $label1 != "exhaust" ]; then
-		echo "0" > $fan2_control_file
-		echo "0" > $fan3_control_file
-	fi
-    ;;
+        echo "0" > "${fan_control_file[1]}"
+        if [[ "${label[1]}" != "exhaust" ]]; then
+		    echo "0" > "${fan_control_file[2]}"
+		    echo "0" > "${fan_control_file[3]}"
+	    fi
+        ;;
 
-    ####  HDD CONTROL
-    hdd)
-        if [ $label1 = "hdd" ]; then
-            function_fan1
-        fi
-
-
-        if [ $label2 =  "hdd" ]; then
-            function_fan2
-
-        fi
-
-        if [ $label3 =  "hdd" ]; then
-            function_fan3
-
-        fi
-    ;;
-
-    ####  CPU CONTROL
-    cpu)
-        if [ $label1 = "cpu" ]; then
-            function_fan1
-        fi
-
-
-        if [ $label2 =  "cpu" ]; then
-            function_fan2
-
-        fi
-
-        if [ $label3 =  "cpu" ]; then
-            function_fan3
-
-        fi
-    ;;
-
-    ####  ODD CONTROL
-    odd)
-        if [ $label1 = "odd" ]; then
-            function_fan1
-        fi
-
-
-        if [ $label2 =  "odd" ]; then
-            function_fan2
-
-        fi
-
-        if [ $label3 =  "odd" ]; then
-            function_fan3
-
-        fi
-
-    ;;
+    ####  HDD/CPU/ODD CONTROL
+    hdd|cpu|odd)
+        for i in 1 2 3; do
+            if [ "${label[$i]}" = "$command" ]; then
+                fan_function "$i"
+            fi
+        done
+        ;;
 
     ### EXHAUST CONTROL
-    exhaust)
-       function_fan1
-    ;;
+    exhaust|master)
+        fan_function 1
+        ;;
 
     ### MASTER CONTROL
-    master)
-        function_fan1
-    ;;
+    #master)
+    #    fan_function 1
+    #    ;;
 esac
